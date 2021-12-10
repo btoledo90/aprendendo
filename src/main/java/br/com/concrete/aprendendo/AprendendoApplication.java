6b4c1f63159e0607490cb5a46ab7608e87d0223b
btoledo90 <brunotq19@hotmail.com>
2021-12-10 17:35:18 -0300

    Commit inicial

---
+++ src/main/java/br/com/concrete/aprendendo/AprendendoApplication.java
@@ -0,0 +1,13 @@
+package br.com.concrete.aprendendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoApplication.class, args);
+	}
+
+}
