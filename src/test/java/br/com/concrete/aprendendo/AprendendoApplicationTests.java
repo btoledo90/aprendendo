45abf42c8b64ab2ba72faa269e768be1f7376276
btoledo90 <brunotq19@hotmail.com>
2021-12-10 17:35:18 -0300

    Commit inicial

---
+++ src/test/java/br/com/concrete/aprendendo/AprendendoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.concrete.aprendendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
